e69c03dfe40a839b6053443455942826c2177d97
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-03-12 13:53:35 +0500

    Week2_Task2

---
+++ Manuals/Week 2/Calculator Class/Calculator Class/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Class
+{
+    internal class Calculator
+    {
+        double num1;
+        double num2;
+        public Calculator(double x, double y)
+        {
+            num1 = x;
+            num2 = y;
+        }
+
+        public double Add()
+        {
+            return num1 + num2;
+        }
+        public double Subtract()
+        {
+            return num1 - num2;
+        }
+        public double Multiply()
+        {
+            return num1 * num2;
+        }
+        public double Divide()
+        {
+            return num1 / num2;
+        }
+
+    }
+}
---
+++ Manuals/Week 2/Calculator Class/Calculator Class/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator( 4.5,5.8);
+            Console.WriteLine("Addition: " + c.Add());
+            Console.WriteLine("Subtraction: " + c.Subtract());
+            Console.WriteLine("Multiplication: " + c.Multiply());
+            Console.WriteLine("Divide: " + c.Divide());
+        }
+    }
+}
