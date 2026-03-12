37566a1da18781839f0c395fe2533817d9df44a0
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-03-12 15:50:25 +0500

    Week3_Task1

---
+++ Manuals/Week 3/Clock Type/Clock Type/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clockType clock1 = new clockType(10, 30, 20);
+            clockType clock2 = new clockType(14, 10, 5);
+
+            Console.Write("Clock 1 time: ");
+            clock1.DisplayTime();
+
+            Console.Write("Clock 2 time: ");
+            clock2.DisplayTime();
+
+            Console.WriteLine("Elapsed Time Clock1 (sec): " + clock1.ElapsedTime());
+
+            Console.WriteLine("Remaining Time Clock1 (sec): " + clock1.RemainingTime());
+
+            Console.WriteLine("Difference Between Clocks (sec): " + clock1.Difference(clock2));
+        }
+    }
+}
---
+++ Manuals/Week 3/Clock Type/Clock Type/clockType.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock_Type
+{
+    internal class clockType
+    {
+        public int hours;
+        public int minutes;
+        public int seconds;
+
+        public clockType(int hrs, int mins, int sec)
+        {
+            hours = hrs;
+            minutes = mins;
+            seconds = sec;
+        }
+
+        public int ElapsedTime()
+        {
+            return ((hours * 3600) + (minutes * 60) + seconds);
+        }
+         
+        public int RemainingTime()
+        {
+            int totalSeconds = 24 * 3600;
+            return totalSeconds - ElapsedTime();
+        }
+
+        public int Difference(clockType otherClock)
+        {
+            int t1 = this.ElapsedTime();
+            int t2 = otherClock.ElapsedTime();
+
+            return Math.Abs(t1 - t2);
+        }
+
+        public void DisplayTime()
+        {
+            Console.WriteLine(hours + " : " + minutes + " : " + seconds);
+        }
+    }
+
+}
