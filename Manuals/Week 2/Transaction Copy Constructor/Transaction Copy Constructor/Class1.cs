06613eaddb06689de3e59cb040d012252a911edd
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-03-12 13:48:31 +0500

    Week1_Task1

---
+++ Manuals/Week 2/Transaction Copy Constructor/Transaction Copy Constructor/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transaction_Copy_Constructor
+{
+    internal class Transaction
+    {
+        public int TransactionId;
+        public string ProductName;
+        public double Amount;
+        public DateTime DateTime;
+
+        //parametrized constructor
+        public Transaction(int id,string name,double amount,DateTime dt)
+        {
+            TransactionId = id;
+            ProductName = name;
+            Amount = amount;
+            DateTime = dt;
+        }
+
+        //copy constructor
+        public Transaction(Transaction t)
+        {
+            TransactionId = t.TransactionId;
+            ProductName = t.ProductName;
+            Amount = t.Amount;
+            DateTime = t.DateTime;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Transaction ID:  " + TransactionId);
+            Console.WriteLine("Product Name: " + ProductName);
+            Console.WriteLine("Amount: " + Amount);
+            Console.WriteLine("Date and Time: " + DateTime);
+        }
+    }
+}
---
+++ Manuals/Week 2/Transaction Copy Constructor/Transaction Copy Constructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transaction_Copy_Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //making first object
+            Transaction t1 = new Transaction(21, "Shoes", 2000, DateTime.Now);
+
+            //copy constructor
+            Transaction t2 = new Transaction(t1);
+
+            Console.WriteLine(" Original Transaction");
+            t1.Display();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Copied Transaction");
+            t2.Display();
+        }
+    }
+}
