44555794f6287ae4675e5b537d052807acfb9912
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-03-12 13:59:15 +0500

    Week2_Task5

---
+++ Manuals/Week 2/Product Management System/Product Management System/Product.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Management_System
+{
+    internal class Product
+    {
+        int count;
+        string []ID = new string[50];
+        string []Name = new string[50];
+        double[] Price = new double[50];
+        string[] Category = new string[50];
+        string[] Brand = new string[50];
+        string[] Country = new string[50];
+
+        public void AddProduct()
+        {
+            Console.Write("Enter Product ID: ");
+            ID[count] = Console.ReadLine();
+
+            Console.Write("Enter Product Name: ");
+            Name[count] = Console.ReadLine();
+
+            Console.Write("Enter Product Price: ");
+            Price[count] = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter Product Category: ");
+            Category[count] = Console.ReadLine();
+
+            Console.Write("Enter Product Brand: ");
+            Brand[count] = Console.ReadLine();
+
+            Console.Write("Enter Product Country: ");
+            Country[count] = Console.ReadLine();
+
+            Console.WriteLine();
+
+            count++;
+        }
+
+        public void ShowProduct() 
+        {
+            if (count==0)
+            {
+                Console.WriteLine("No Product Found!!!");
+            }
+            else
+            {
+                string SearchID = Console.ReadLine();
+                for (int i=0;i<count;i++)
+                {
+                    if (SearchID == ID[i])
+                    
+                    Console.WriteLine($"Product Name: {Name[i]}");
+                    Console.WriteLine($"Product Price: {Price[i]}");
+                    Console.WriteLine($"Product Category: {Category[i]}");
+                    Console.WriteLine($"Product Brand: {Brand[i]}");
+                    Console.WriteLine($"Product Country: {Country[i]}\n\n");
+                }
+            }
+        }
+
+        public void TotalStoreWorth()
+        {
+            if (count == 0)
+            {
+                Console.WriteLine("No Product Found!!!");
+            }
+            else
+            {
+                double totalWorth = 0;
+                for(int i = 0; i < count; i++)
+                {
+                    totalWorth = totalWorth + Price[i];
+                     
+                }
+                Console.WriteLine($"Product Price: {totalWorth}");
+            }
+            
+        }
+    }
+}
---
+++ Manuals/Week 2/Product Management System/Product Management System/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Product_Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            Product p = new Product();
+            /*Create Product class with :
+             • ID 
+             • Name 
+             • Price 
+             • Category 
+             • Brand 
+             • Country
+                  Functions: 
+             • Add Product 
+             • Show Product 
+             • Total Store Worth*/
+            Console.WriteLine("PRODUCT MANAGEMENT SYSTEM");
+
+            do
+            {
+                Console.WriteLine("1.Add Product");
+                Console.WriteLine("2.Show Product");
+                Console.WriteLine("3.Total Store Worth");
+                Console.WriteLine("4.Exit\n");
+
+                Console.Write("Enter your choice: ");
+
+                choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    p.AddProduct();
+                }
+                else if(choice == 2)
+                {
+                    p.ShowProduct();
+                }
+                else if (choice == 3)
+                {
+                    p.TotalStoreWorth();
+                }
+            } while (choice != 4);
+
+
+    }
+    }
+}
