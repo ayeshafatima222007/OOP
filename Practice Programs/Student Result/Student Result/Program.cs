be3acaf958ae2d7ec146c017cc43978462913bee
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-03-13 17:31:17 +0500

    Student Result using list and class

---
+++ Practice Programs/Student Result/Student Result/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Result
+{
+    internal class Program
+    {
+        static Student takeInput()
+        {
+            Student s = new Student();
+
+            Console.Write("Enter Name: ");
+            s.sname = Console.ReadLine();
+
+            Console.Write("Enter Urdu Marks: ");
+            s.urduMarks = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter English Marks: ");
+            s.engMarks = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter Maths Marks: ");
+            s.mathsMarks = float.Parse(Console.ReadLine());
+
+            return s;
+        }
+
+        static void sum(Student s)
+        {
+            s.total = s.engMarks + s.urduMarks + s.mathsMarks;
+        }
+
+        static void output(Student s)
+        {
+            Console.Write(s.sname);
+            Console.Write("\t");
+            Console.WriteLine(s.total);
+        }
+
+        static void Main(string[] args)
+        {
+            List<Student> stu = new List<Student>();
+
+            for (int x = 0; x < 3; x++)
+            {
+                Student temp = takeInput();
+                sum(temp);
+                stu.Add(temp);
+            }
+
+            foreach (Student s in stu)
+            {
+                output(s);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Practice Programs/Student Result/Student Result/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Result
+{
+    internal class Student
+    {
+        public string sname;
+        public float urduMarks;
+        public float engMarks;
+        public float mathsMarks;
+        public float total;
+
+    }
+}
