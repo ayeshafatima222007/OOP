027d880bbc5d4c20e3ee1bd98c2e0ab2266772c6
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-03-19 22:21:06 +0500

    Week4_SelfAssesment1

---
+++ Manuals/Week 4/Merit & Scholorship/Merit & Scholorship/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace self_assessment_task_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> studentList = new List<Student>();
+            string name;
+            int rollNumber;
+            float cgpa;
+            int semFee;
+            int matricMarks;
+            string hometown;
+            bool isHostelite;
+            //----------
+            float f;
+            float e;
+            while (true)
+            {
+                int choice = 0;
+                Console.WriteLine(" <<< MENU >>> ");
+                Console.WriteLine("1. Register a Student");
+                Console.WriteLine("2. Calculate Aggregate");
+                Console.WriteLine("3. Scholarship Status");
+                Console.WriteLine("4. Display Information");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Enter your Choice: ");
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    if(choice==1)
+                    {
+                        //collecting information
+                        Console.Write("Enter Student's name: ");
+                        name = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.Write("Enter Roll Number: ");
+                        rollNumber = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.Write("Enter CGPA: ");
+                        cgpa = float.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.Write("Enter Semester Fee: ");
+                        semFee = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.Write("Enter Matric Marks: ");
+                        matricMarks = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.Write("Enter FSC Marks: ");
+                        f = float.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.Write("Enter ECAT marks: ");
+                        e = float.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.Write("Enter hometown: ");
+                        hometown = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.Write("Hostelite (True/False): ");
+                        isHostelite = bool.Parse(Console.ReadLine());
+                        Console.WriteLine("-----------------------------");
+                        //create object
+                        Student s = new Student(name, rollNumber, cgpa, semFee, matricMarks, f, e, hometown, isHostelite);
+                        //add object to the list
+                        studentList.Add(s);
+                    }
+                    if (choice == 2)
+                    {
+                        foreach (Student s in studentList)
+                        {
+                            Console.WriteLine(" <<< AGGREGATE >>>");
+                            s.calculateMerit();
+                        }
+                    }
+                    else if (choice == 3)
+                    {
+                        foreach (Student s in studentList)
+                        {
+                            if (s.isEligibleforScholarship() == true)
+                            {
+                                Console.WriteLine(" Student is eligible for scholarship");
+                            }
+                            else if (s.isEligibleforScholarship() == false)
+                            {
+                                Console.WriteLine(" Student is NOT eligible for scholarship");
+                            }
+                        }
+                    }
+                    else if (choice == 4)
+                    {
+                        foreach (Student s in studentList)
+                        {
+                            s.display();
+                        }
+                    }
+                    else if (choice == 5)
+                    {
+                        Console.WriteLine("Exiting");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Manuals/Week 4/Merit & Scholorship/Merit & Scholorship/Student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace self_assessment_task_01
+{
+    internal class Student
+    {
+        public string name;
+        public int rollNumber;
+        public float cgpa;
+        public int semFee;
+        public int matricMarks;
+        // ----------------
+        //calculated will be here in these three
+        public float fscMarks;
+        public float ecatMarks;
+        //------------------
+        public string hometown;
+        public bool isHostelite;
+        public bool isTakingScholarship;
+        float agg;
+
+        //parameterized constructor to set values
+        public Student(string name, int rollNumber, float cgpa, int semFee, int matricMarks, float f, float e, string hometown, bool isHostelite)
+        {
+            this.name = name;
+            this.rollNumber = rollNumber;
+            this.cgpa = cgpa;
+            this.semFee = semFee;
+            this.matricMarks = matricMarks;
+            this.hometown = hometown;
+            this.isHostelite = isHostelite;
+            this.fscMarks = f;
+            this.ecatMarks = e;
+        }
+        //method that will calculate the aggregate
+        public float calculateMerit()
+        {
+            float fpercentage = (fscMarks / 1200) * 60;
+            float epercentage= (ecatMarks / 400) * 40;
+            agg = fpercentage + epercentage;
+            Console.WriteLine("Aggregate : "+agg);
+            return agg;
+        }
+        //method to check eligibility for scholarship
+        public bool isEligibleforScholarship()
+        {
+            if (agg >= 80)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //method to display all information
+        public void display()
+        {
+            Console.WriteLine(" <<< STUDENT INFO >>>");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Roll Number: " + rollNumber);
+            Console.WriteLine("CGPA: " + cgpa);
+            Console.WriteLine("Semester Fee: " + semFee);
+            Console.WriteLine("Matric Marks: " + matricMarks);
+            Console.WriteLine("FSC Marks: " + fscMarks);
+            Console.WriteLine("ECAT Marks: " + ecatMarks);
+            Console.WriteLine("HomeTown: " + hometown);
+            Console.WriteLine("Accommodation Status: " + isHostelite);
+            Console.WriteLine("Scholarship Status: " + isTakingScholarship);
+            Console.WriteLine("----------------------------------------");
+        }
+    }
+}
