b76eec92f0dc21c0e53819689259a0c81c5cd8c3
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-03-19 22:02:13 +0500

    Week4-Ch1

---
+++ Manuals/Week 4/Challenge-01/Challenge-01/Book.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Challenge_01
+{
+    internal class Book
+    {
+        public string title;
+        public string[] authors=new string[4];
+        public int authorCount; //track the no. of authors added
+        public string publisher;
+        public string isbn;
+        public int price;
+        public int stock;
+        //parameterized constructor to set the values
+        public Book(string title, string[] authors, int authorCount, string publisher, string isbn, int price, int stock)
+        {
+            this.title = title;
+            this.authors = authors;
+            this.authorCount = authorCount;
+            this.publisher = publisher;
+            this.isbn = isbn;
+            this.price = price;
+            this.stock = stock;
+        }
+
+        // ----------------methods for title----------------
+        //show title
+        public void showTitle()
+        {
+            Console.WriteLine("------------------------");
+            Console.WriteLine($" Title : {title}");
+        }
+        //set title
+        public void setTitle(string t)
+        {
+            title = t;
+        }
+        //get title
+        public string getTitle()
+        {
+            return title;
+        }
+        //Search by title
+        public bool searchTitle(string t)
+        {
+            if(title.ToLower()==t.ToLower())
+            {
+                return true;
+            }
+            return false;
+        }
+        // ----------------methods for stock----------------
+
+        //show stock
+        public void showStock()
+        {
+            Console.WriteLine("------------------------");
+            Console.WriteLine($" Stock : {stock}");
+
+        }
+        //update stock 
+        public int updateStock(int s)
+        {
+            stock = stock + s;
+            return stock;
+        }
+        //set stock
+        public void setStock(int s)
+        {
+            stock = s;
+        }
+        //get stock
+        public int getStock()
+        {
+            return stock;
+        }
+        // ----------------methods for publisher ---------------
+        //show publisher
+        public void showpublisher()
+        {
+            Console.WriteLine("------------------------");
+            Console.WriteLine($" Publisher : {publisher}");
+
+        }
+        //set publisher
+        public void setPublisher(string p)
+        {
+            publisher = p;
+        }
+        //get publisher
+        public string getPublisher()
+        {
+            return publisher;
+        }
+        // ----------------methods for ISBN----------------
+        //show isbn
+        public void showISBN()
+        {
+            Console.WriteLine("------------------------");
+            Console.WriteLine($" ISBN : {isbn}");
+
+        }
+        //search by isbn
+        public bool searchISBN(string i)
+        {
+            if(isbn.ToLower()==i.ToLower())
+            {
+                return true ;
+            }
+            return false;
+        }
+        //set isbn
+        public void setISBN(string i)
+        {
+            isbn = i;
+        }
+        // ----------------methods for price ----------------
+        //show price
+        public void showPrice()
+        {
+            Console.WriteLine("------------------------");
+            Console.WriteLine($" Price : {price}");
+
+        }
+        //set price
+        public void setPrice(int p)
+        {
+            price = p;
+        }
+        //get price
+        public int getPrice()
+        {
+            return price;
+        }
+        // ----------------methods for authors ----------------
+
+        //show author
+        public void showAuthor()
+        {
+            Console.WriteLine("------------------------");
+            for (int i = 0; i < authorCount; i++)
+            {
+                Console.WriteLine($"Author {i + 1} is: {authors[i]}");
+            }
+        }
+        //add author to array
+        public void addAuthor(String name)
+        {
+            if(authorCount<4)
+            {
+                authors[authorCount] = name;
+                authorCount++;
+                Console.WriteLine("Author added !");
+            }
+            else
+            {
+                Console.WriteLine("Error: Maximum of 4 authors reached!");
+            }
+        }
+        //get author
+        public int getAuthorCount()
+        {
+            return authorCount;
+        }
+        //----------------display all ----------------
+        public void displayAll()
+        {
+            showTitle();
+            showAuthor();
+            showpublisher();
+            showISBN();
+            showPrice();
+            showStock();
+        }
+    }
+}
---
+++ Manuals/Week 4/Challenge-01/Challenge-01/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Challenge_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //array of object, each object got deals with one book
+            Book[] books = new Book[100];
+            int totalBooks = 0; //counter
+            //-----
+            string title;
+            string publisher;
+            string isbn;
+            int price;
+            int stock;
+            //-----
+
+            while (true)
+            {
+                int choice = 0;
+                Console.WriteLine("----------------------");
+                Console.WriteLine("     <<< MENU >>>     ");
+                Console.WriteLine("----------------------");
+                Console.WriteLine("1. Add Book ");
+                Console.WriteLine("2. Search by Title");
+                Console.WriteLine("3. Search by ISBN ");
+                Console.WriteLine("4. Update Book Info");
+                Console.WriteLine("5. Display All Books");
+                Console.WriteLine("6. Exit ");
+                Console.Write("Enter Your Choice: ");
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    if (choice == 1)
+                    {
+                        if (totalBooks >= 100)
+                        {
+                            Console.WriteLine("Library is full!");
+                            continue;
+                        }
+                        //collecting info
+                        Console.Write("Enter Book's Title: ");
+                        title = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.Write("Enter publisher: ");
+                        publisher = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.Write("Enter ISBN: ");
+                        isbn = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.Write("Enter Price: ");
+                        price = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.Write("Enter Stock: ");
+                        stock = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        //multiple authors
+                        string[] tempAuthors = new string[4];
+                        int count = 0;
+                        Console.Write("How many authors (1-4)? : ");
+                        if (int.TryParse((string)Console.ReadLine(), out count) && count > 0 && count < 5)
+                        {
+                            for (int j = 0; j < count; j++)
+                            {
+                                Console.Write($"Enter Author {j + 1} Name: ");
+                                tempAuthors[j] = Console.ReadLine();
+                            }
+                        }
+                        //new object
+                        books[totalBooks] = new Book(title, tempAuthors,count, publisher, isbn, price, stock);
+                        totalBooks++;
+                        Console.WriteLine("Book Added ! ");
+                    }
+                    else if (choice == 2)
+                    {
+                        if (totalBooks == 0)
+                        {
+                            Console.WriteLine("Library is empty ");
+                            continue;
+                        }
+                        //search by title
+                        Console.Write("Enter Search Term: ");
+                        string search = Console.ReadLine();
+                        bool found = false;
+                        for (int j = 0; j < totalBooks; j++)
+                        {
+                            if (books[j].searchTitle(search) == true)
+                            {
+                                Console.WriteLine(" << BOOK FOUND >>");
+                                found = true;
+                                books[j].displayAll();
+                            }
+                            else
+                            {
+                                found = false;
+                            }
+                        }
+                        if (found == false)
+                        {
+                            Console.WriteLine("Book not found ! ");
+                        }
+                    }
+                    else if (choice == 3)
+                    {
+                        if (totalBooks == 0)
+                        {
+                            Console.WriteLine("Library is empty ");
+                            continue;
+                        }
+                        //search by isbn
+                        Console.Write("Enter ISBN: ");
+                        string search = Console.ReadLine();
+                        bool found = false;
+                        for (int j = 0; j < totalBooks; j++)
+                        {
+                            if (books[j].searchISBN(search) == true)
+                            {
+                                Console.WriteLine(" << BOOK FOUND >>");
+                                found = true;
+                                books[j].displayAll();
+                            }
+                            else
+                            {
+                                found = false;
+                            }
+                        }
+                        if(found==false)
+                        {
+                            Console.WriteLine("Book not found ! ");
+                        }
+                    }
+                    else if (choice == 4)
+                    {
+                        if (totalBooks == 0)
+                        {
+                            Console.WriteLine("Library is empty ");
+                            continue;
+                        }
+                        //update stock
+                        //search by isbn
+                        Console.Write("Enter ISBN: ");
+                        string search = Console.ReadLine();
+                        for (int j = 0; j < totalBooks; j++)
+                        {
+                            if (books[j].searchISBN(search) == true)
+                            {
+                                Console.Write("Add how many copies? ");
+                                int amount = int.Parse(Console.ReadLine());
+                                books[j].updateStock(amount);
+                                Console.WriteLine("Stock Updated!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Book not found ! ");
+                            }
+                        }
+                    }
+                    else if (choice == 5)
+                    {
+                        if (totalBooks == 0)
+                        {
+                            Console.WriteLine("Library is empty ");
+                            continue;
+                        }
+                        for (int j = 0; j < totalBooks; j++)
+                        {
+                            books[j].displayAll();
+                            Console.WriteLine("-----------------------");
+                        }
+                    }
+                    else if (choice == 6)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("[ERROR]: Please enter no. (1-6) ");
+                }
+            }
+        }
+    }
+}
