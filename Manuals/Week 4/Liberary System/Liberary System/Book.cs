1281a2eff17a8f1a3586bb4bdb2a0965aa8d1fb1
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-03-19 22:21:52 +0500

    Week4_SelfAssesment2

---
+++ Manuals/Week 4/Liberary System/Liberary System/Book.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace self_assessment_task_02
+{
+    internal class Book
+    {
+        public string title;
+        public List<string> chapters;
+     
+        public int pages;
+        public int price;
+        public string author;
+        public bool isavailable;
+        public int bookMark;
+
+         
+        public Book(string title,List<string> chapters, int pages, int prices, string author, bool isavailable, int bookMark)        //parameterized constructor
+        {
+            this.title= title;
+            this.chapters = chapters;
+            this.pages = pages;
+            this.price = prices;
+            this.author = author;
+            this.isavailable = isavailable;
+            this.bookMark = bookMark;
+        }
+         
+        public bool isBookAvailable()               //check if book is available
+        {
+            return isavailable;
+        }
+
+         
+        public string getChapter(int chapterNumber)        //get the name of chapter  
+        {
+            int index=chapterNumber-1;
+            if (index >= 0 && index < chapters.Count)
+            {
+
+                return chapters[index];
+            }
+            else
+            {
+                return "Chapter not found ! ";
+            }
+        }
+        
+        public int getBookMark()
+        {
+            Console.WriteLine("Bookmark set at page: " + bookMark);
+            return bookMark;
+        }
+    
+        public void displayBookInfo()
+        {
+            Console.WriteLine($"\n--- {title} ---");
+            Console.WriteLine($"Author: {author} | Pages: {pages} | Price: {price}");
+            Console.WriteLine($"Availability: {isavailable}");
+            Console.WriteLine($"Bookmark at page: {bookMark}");
+        }
+    }
+}
---
+++ Manuals/Week 4/Liberary System/Liberary System/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace self_assessment_task_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string title;
+            List<Book> libraryList = new List<Book>();
+            int pages;
+            int prices;
+            string author;
+            bool isavailable;
+            int bookMark;
+
+           
+            while (true)
+            {
+                int choice = 0;
+                Console.WriteLine("============= LIBRARY MENU =========");
+                Console.WriteLine("1. Add a Book");
+                Console.WriteLine("2. Check Availibility");
+                Console.WriteLine("3. Search Chapter ");
+                Console.WriteLine("4. View All Books");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your Choice: ");
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    if (choice == 1)
+                    {
+                        Console.Write("Enter Book's title: ");
+                        title = Console.ReadLine();
+
+                        Console.Write("Enter author name: ");
+                        author = Console.ReadLine();
+
+                        Console.Write("Enter no. of pages: ");
+                        pages = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter price: ");
+                        prices = int.Parse(Console.ReadLine());
+
+                        Console.Write("Available (true/false): ");
+                        isavailable = bool.Parse(Console.ReadLine());
+
+                        Console.Write("Enter Bookmark: ");
+                        bookMark = int.Parse(Console.ReadLine());
+
+
+                        List<string> tempchapters = new List<string>();
+                        Console.Write("How many chapters? : ");
+                        int count = int.Parse(Console.ReadLine());
+
+                        for (int i = 0; i < count; i++)
+                        {
+                            Console.Write($"Enter name for Chapter {i + 1}: ");
+                            string chName = Console.ReadLine();
+                            tempchapters.Add(chName);
+                        }
+
+                        Book newbook = new Book(title, tempchapters, pages, prices, author, isavailable, bookMark);           //object of book
+                        libraryList.Add(newbook);
+                    }
+                    else if (choice == 2)
+                    {
+
+                        foreach (Book b in libraryList)
+                        {
+                            Console.WriteLine("Availability Status: {0} ", b.isBookAvailable());
+                            Console.WriteLine("-----------------------------------");
+                        }
+                    }
+                    else if (choice == 3)
+                    {
+                        Console.Write("Enter the title of the book you want to search: ");
+                        string searchTitle = Console.ReadLine();
+                        bool found = false;
+
+                        foreach (Book b in libraryList)
+                        {
+                            if (b.title.ToLower() == searchTitle.ToLower()) // Search logic
+                            {
+                                Console.WriteLine("Enter chapter no. to search: ");
+                                int chapno = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Chapter: {0}", b.getChapter(chapno));
+                                found = true;
+                                break;
+                            }
+                        }
+
+                        if (!found)
+                        {
+                            Console.WriteLine("Book not found in library.");
+                        }
+                    }
+
+                    else if (choice == 4)
+                    {
+                        foreach (Book b in libraryList)
+                        {
+                            b.displayBookInfo();
+                        }
+                    }
+
+                    else if (choice == 5)
+                    {
+                        return;
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("--Invalid Choice-- ");
+                }
+            }
+        }
+    }
+}
