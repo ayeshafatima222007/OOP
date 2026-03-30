f300ad8276fb2f38dd4ad5baa67f3d904a439a12
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-03-30 21:01:59 +0500

    Week5_Ch1

---
+++ Manuals/Week 5/2D Point/2D Point/MyPoint.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_Point
+{
+    internal class MyPoint
+    {
+        public int x;
+        public int y;
+
+        public MyPoint()     //Default constructor
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public MyPoint(int x,int y)       //Parametrized constructor
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+
+        public void setX(int x)
+        {
+            this.x = x;
+        }
+
+        public void setY(int y)
+        {
+            this.y = y;
+        }
+
+        public void setXY(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double distanceWithCords(int x,int y)
+        {
+            return Math.Sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y));
+        }
+
+        public double distanceWithObject(MyPoint P)
+        {
+
+        }
+
+        public double distanceFromZero()
+        {
+
+        }
+
+    }
+}
