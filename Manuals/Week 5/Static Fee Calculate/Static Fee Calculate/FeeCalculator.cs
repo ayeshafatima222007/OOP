98c3f74ca819b7cb79f3ff405f4668443100e513
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-04-01 23:20:30 +0500

    Static Functions Example

---
+++ Manuals/Week 5/Static Fee Calculate/Static Fee Calculate/FeeCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Fee_Calculate
+{
+    internal class FeeCalculator
+    {
+        public static double calculateFee(double creditHours)
+        {
+            return creditHours * 5000;
+        }
+    }
+}
---
+++ Manuals/Week 5/Static Fee Calculate/Static Fee Calculate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Fee_Calculate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double fee = FeeCalculator.calculateFee(3);
+
+            Console.WriteLine("Total Fee: " + fee);
+        }
+    }
+}
