382faa623d4d67f01f174d6450816367a7b688c0
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-04-01 23:25:17 +0500

    Week5_Ch1

--- Manuals/Week 5/2D Point/2D Point/Program.cs
+++ Manuals/Week 5/2D Point/2D Point/Program.cs
@@ -10,8 +10,14 @@ namespace _2D_Point
     {
         static void Main(string[] args)
         {
+            MyLine line = null;
+
             while (true)
             {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("-          2D Point Model       -");
+                Console.WriteLine("---------------------------------");
+
                 Console.WriteLine("1. Make a Line");
                 Console.WriteLine("2. Update the begin point");
                 Console.WriteLine("3. Update the end point");
@@ -27,9 +33,10 @@ namespace _2D_Point
                 int option;
                 while (true)
                 {
+                    Console.Write("Enter option: ");
                     if (int.TryParse(Console.ReadLine(), out option))
                     {
-                        Console.Write("Enter option: ");
+                        break;
                     }
                     else
                     {
@@ -54,49 +61,138 @@ namespace _2D_Point
                     int ex = int.Parse(Console.ReadLine());
 
                     Console.Write("Enter y of line end");     
-                    int Ey = int.Parse(Console.ReadLine());
+                    int ey = int.Parse(Console.ReadLine());
  
+                    MyPoint xy = new MyPoint(bx, by);         //assigning points
+                    MyPoint XY = new MyPoint(ex, ey);
 
+                    line = new MyLine(xy,XY);               //assigning points to line
 
+                    Console.WriteLine("--Line created!--");
                 }
                 else if (option == 2)
                 {
                     Console.WriteLine("---Update the begin point---");
+
+                    if (line == null)
+                    {
+                        Console.WriteLine("Create a line first");
+                    }
+                    else 
+                    {
+                        Console.Write("Enter new value of x of line begin");       //coordiantes of line begin
+                        int Newbx = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter new valueof y of line begin");
+                        int Newby = int.Parse(Console.ReadLine());
+
+                        MyPoint newBegin = new MyPoint(Newbx,Newby);
+                        line.setBegin(newBegin);
+                    }
                 }
 
                 else if (option == 3)
                 {
                     Console.WriteLine("---Update the end point---");
+
+                    if (line == null)
+                    {
+                        Console.WriteLine("Create a line first");
+                    }
+                    else
+                    {
+                        Console.Write("Enter new value of x of line end");       //coordiantes of line begin
+                        int Newex = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter new valueof y of line end");
+                        int Newey= int.Parse(Console.ReadLine());
+
+                        MyPoint newEnd = new MyPoint(Newex, Newey);
+                        line.setEnd(newEnd);
+                    }
                 }
 
                 else if (option == 4)
                 {
                     Console.WriteLine("---Show the begin point---");
+
+                    if (line == null)
+                    {
+                        Console.WriteLine("Create a line first");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Begin point: {line.getBegin().getX()} ,{line.getBegin().getY()}");
+                    }
+                     
                 }
 
                 else if (option == 5)
                 {
                     Console.WriteLine("---Show the end point---");
+                    if (line == null)
+                    {
+                        Console.WriteLine("Create a line first");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"End point: {line.getEnd().getX()} ,{line.getEnd().getY()}");
+                    }
+
                 }
 
                 else if (option == 6)
                 {
                     Console.WriteLine("---Get the Length of the line---");
+                    if (line == null)
+                    {
+                        Console.WriteLine("Create a line first");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Length of line: " + line.getLength());
+                    }
                 }
 
                 else if (option == 7)
                 {
                     Console.WriteLine("---Get the Gradient of the Line---");
+
+                    if (line == null)
+                    {
+                        Console.WriteLine("Create a line first");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Gradient of line: " + line.getGradient());
+                    }
                 }
 
                 else if (option == 8)
                 {
                     Console.WriteLine("---Distance of begin point from origin---");
+                    if (line == null)
+                    {
+                        Console.WriteLine("Create a line first");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Distance from origin: {line.getBegin().distanceFromZero()}");    //Call distanceFromZero() on the begin point object
+                    }
                 }
 
                 else if (option == 9)
                 {
                     Console.WriteLine("---Distance of end point from origin---");
+
+                    if (line == null)
+                    {
+                        Console.WriteLine("Create a line first");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Distance from origin: {line.getEnd().distanceFromZero()}");          //Call distanceFromZero() on the end point object
+                    }
                 }
 
                 else if (option == 10)
@@ -105,6 +201,7 @@ namespace _2D_Point
                     Console.WriteLine("");
                     Console.WriteLine("Enter any key to exit");
                     Console.ReadKey();
+                    break;
                 }
             }
             
