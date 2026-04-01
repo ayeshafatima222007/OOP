94d3cfdad8494bf4a45c0f82b4838085ba23a04b
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-04-01 23:17:11 +0500

    Static Attribute Example

---
+++ Manuals/Week 5/Total Students/Total Students/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Total_Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Ali");
+            Student s2 = new Student("Sara");
+            Student s3 = new Student("Ahmed");
+
+            Console.WriteLine("Total Students: " + Student.studentCount);
+        }
+    }
+}
---
+++ Manuals/Week 5/Total Students/Total Students/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Total_Students
+{
+    internal class Student
+    {
+        public string name;
+        public static int studentCount = 0;
+
+        public Student(string name)
+        {
+            this.name = name;
+            studentCount++;
+        }
+    }
+}
