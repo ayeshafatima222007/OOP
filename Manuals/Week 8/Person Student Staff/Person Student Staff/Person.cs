cb968e4220bed7739ab8e9057b469fc24863f168
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-05-02 12:18:53 +0500

    Week-8 Ch-3

---
+++ Manuals/Week 8/Person Student Staff/Person Student Staff/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Student_Staff
+{
+    internal class Person
+    {
+        private string name;
+        private string address;
+
+        public Person(string name,string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+
+        public string getNmae()
+        {
+            return name;
+        }
+        public string getAddress()
+        {
+            return address;
+        }
+        public void setAddress(string address)
+        {
+            this.address = address;
+        }
+
+        public string toString()
+        {
+            return $"Person [name = {name} , address = {address}]";
+        }
+    }
+}
---
+++ Manuals/Week 8/Person Student Staff/Person Student Staff/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Student_Staff
+{
+    internal class Student : Person
+    {
+        private string program;
+        private int year;
+        private double fee;
+
+        public Student(string name,string address,string program, int year, double fee) : base (name,address)
+        {
+            //name and address is handled by parent class
+            this.program = program;
+            this.year = year;
+            this.fee = fee;
+        }
+
+        public string getProgram()
+        {
+            return program;
+        }
+
+        public void setProgram(string program)
+        {
+            this.program = program;
+        }
+
+        public int getYear()
+        {
+            return year;
+        }
+
+        public void setYear(int year)
+        {
+            this.year = year;
+        }
+
+        public double getFee()
+        {
+            return fee;
+        }
+
+        public void setFee(double fee)
+        {
+            this.fee = fee;
+        }
+    }
+}
