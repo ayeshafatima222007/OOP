b38c25f5fc91330192e5ca15162e2fd08cef6159
ayeshafatima222007 <ayeshafatima222007@gmail.com>
2026-05-13 22:31:50 +0500

    Self Assessment-03

---
+++ Manuals/Week 10/Text Button Click/Text Button Click/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Text_Button_Click
+{
+    partial class txtDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textDisplay = new System.Windows.Forms.TextBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textDisplay
+            // 
+            this.textDisplay.Location = new System.Drawing.Point(344, 180);
+            this.textDisplay.Name = "textDisplay";
+            this.textDisplay.ReadOnly = true;
+            this.textDisplay.Size = new System.Drawing.Size(158, 26);
+            this.textDisplay.TabIndex = 1;
+            this.textDisplay.Text = "Hello World";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(410, 243);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 31);
+            this.btnShow.TabIndex = 2;
+            this.btnShow.Text = "Show Message";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // txtDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.textDisplay);
+            this.Name = "txtDisplay";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textDisplay;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ Manuals/Week 10/Text Button Click/Text Button Click/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Button_Click
+{
+    public partial class txtDisplay : Form
+    {
+        public txtDisplay()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            textDisplay.Text = "Hello, World!";
+        }
+    }
+}
